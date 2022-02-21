571344c4b18abb48fd33a2f5cea37e3dc11ed14d
Iitu <iitu.tahtinen@student.hamk.fi>
2022-02-21 18:12:58 +0200

    Korjattu ohjelman toiminta jos annetaan väärä henkilötunnus

--- Program.cs
+++ Program.cs
@@ -15,15 +15,16 @@ namespace Palkanlaskenta_projekti
             {
                 Console.WriteLine("Valitse toiminta:");
                 Console.WriteLine("[0] Pois");
-                Console.WriteLine("[1] Lisätä uusi henkilö");
+                Console.WriteLine("[1] Lisää uusi henkilö");
                 Console.WriteLine("[2] Poista henkilö");
                 Console.WriteLine("[3] Tulosta työntekijöiden tiedot");
                 syote = Console.ReadLine();
+                Console.WriteLine("");
 
                 if (syote == "0")
                 {
                     break;
-                }  
+                }
                 if (syote == "1")
                 {
                     Console.WriteLine("Sukunimi:");
@@ -35,6 +36,7 @@ namespace Palkanlaskenta_projekti
 
                     double Bruttopalkka;
                     string input;
+
                     do
                     {
                         Console.WriteLine("Bruttopalkka:");
@@ -44,8 +46,10 @@ namespace Palkanlaskenta_projekti
                             Console.WriteLine("Virheellinen syöte. Kokeile uudelleen.");
                         }
                     }
+
                     while (Bruttopalkka <= 0);
-                      
+                    Console.WriteLine("");
+
                     string rivi = Sukunimi + ";" + Etunimi + ";" + Henkilotunnus + ";" + Bruttopalkka;
 
                     // Tie tiedostoon henkilokunta.csv, joka sijaitsee projektin kansiossa
@@ -54,8 +58,8 @@ namespace Palkanlaskenta_projekti
                     var teksti = "123";
 
                     // Varmistaa halutaanko tiedot lisätä tiedostoon
-                    
-                    while (teksti != "K" || teksti != "E") 
+
+                    while (teksti != "K" || teksti != "E")
                     {
                         Console.WriteLine("Haluatko lisätä seuraavat tiedot tiedostoon:" + rivi);
                         Console.WriteLine("[K] Kyllä");
@@ -69,61 +73,79 @@ namespace Palkanlaskenta_projekti
                             {
                                 sw.WriteLine(rivi);
                             }
+                            Console.WriteLine("");
                             Console.WriteLine("Tiedot lisätty tiedostoon: " + tie);
                             Console.WriteLine("");
                             break;
                         }
                         else if (teksti == "E")
                         {
+                            Console.WriteLine("");
                             Console.WriteLine("Tietoja ei tallennettu.");
                             Console.WriteLine("");
                             break;
                         }
                     }
-                          
+
                 }
                 if (syote == "2")
                 {
-
                     Console.WriteLine("Anna poistettavan henkilön henkilötunnus:");
                     string Henkilotunnus = Console.ReadLine();
-                    Console.WriteLine("Haluatko varmasti poistaa henkilön: " + Henkilotunnus);
-                    Console.WriteLine("[K] Kyllä");
-                    Console.WriteLine("[E] Ei");
-                    var teksti = Console.ReadLine();
 
-                    if (teksti == "K")
+                    string tie = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, Path.GetFileName("henkilokunta.csv"));
+                    string content = File.ReadAllText(tie);
+
+                    if (content.IndexOf(Henkilotunnus) > -1) // Tarkastaa löytyykö henkilötunnus tiedostosta
                     {
-                        string tie = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, Path.GetFileName("henkilokunta.csv"));
-                        string[] Lines = File.ReadAllLines(tie);
-                        File.Delete(tie);// Deleting the file
-                        using (StreamWriter sw = File.AppendText(tie))
+                        Console.WriteLine("");
+                        Console.WriteLine("Haluatko varmasti poistaa henkilön: " + Henkilotunnus, "\n");
+                        Console.WriteLine("[K] Kyllä");
+                        Console.WriteLine("[E] Ei");
+                        var teksti = Console.ReadLine();
 
+                        if (teksti == "K")
                         {
-                            foreach (string line in Lines)
+
+                            string[] Lines = File.ReadAllLines(tie);
+                            File.Delete(tie);// Poistaa tiedoston
+                            using (StreamWriter sw = File.AppendText(tie))
+
                             {
-                                if (line.IndexOf(Henkilotunnus) >= 0)
+                                foreach (string line in Lines)
                                 {
-                                    //Skip the line
-                                    continue;
+                                    if (line.IndexOf(Henkilotunnus) >= 0)
+                                    {
+                                        //Skippaa rivin 
+                                        continue;
+                                    }
+                                    else
+                                    {
+                                        sw.WriteLine(line);
+                                    }
                                 }
-                                else
-                                {
-                                    sw.WriteLine(line);
-                                }
+                                Console.WriteLine("");
+                                Console.WriteLine("Henkilön tiedot poistettu");
+                                Console.WriteLine("");
                             }
-                            Console.WriteLine("Henkilön tiedot poistettu");
+
+                        }
+                        else if (teksti == "E")
+                        {
+                            Console.WriteLine("");
+                            Console.WriteLine("Poisto peruutettu");
                             Console.WriteLine("");
                         }
-
-
                     }
-                    else if (teksti == "E")
+                    else
                     {
-                        Console.WriteLine("Poisto peruutettu");
+                        Console.WriteLine("");
+                        Console.WriteLine("Henkilöä ei löydy");
                         Console.WriteLine("");
                     }
                 }
+
+
                 if (syote == "3")
                 {
                     string tie = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, Path.GetFileName("henkilokunta.csv"));
