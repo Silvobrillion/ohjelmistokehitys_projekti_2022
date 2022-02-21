83ed7945dcb3ae890e201a860879b97b6f3aebdc
Izabel Silvola <izabel.silvola@student.hamk.fi>
2022-02-21 16:18:16 +0200

    Syötebugit korjattu (osittain)

--- Program.cs
+++ Program.cs
@@ -8,20 +8,23 @@ namespace Palkanlaskenta_projekti
     class MainClass
     {
 
-
         public static void Main(string[] args)
         {
-            int syote = 10;
-            while (syote != 0)
+            string syote = "99";
+            while (syote != "0")
             {
                 Console.WriteLine("Valitse toiminta:");
                 Console.WriteLine("[0] Pois");
                 Console.WriteLine("[1] Lisätä uusi henkilö");
                 Console.WriteLine("[2] Poista henkilö");
                 Console.WriteLine("[3] Tulosta työntekijöiden tiedot");
-                syote = Int32.Parse(Console.ReadLine());
+                syote = Console.ReadLine();
 
-                if (syote == 1)
+                if (syote == "0")
+                {
+                    break;
+                }  
+                if (syote == "1")
                 {
                     Console.WriteLine("Sukunimi:");
                     string Sukunimi = Console.ReadLine();
@@ -29,37 +32,57 @@ namespace Palkanlaskenta_projekti
                     string Etunimi = Console.ReadLine();
                     Console.WriteLine("Henkilötunnus:");
                     string Henkilotunnus = Console.ReadLine();
-                    Console.WriteLine("Bruttopalkka:");
-                    double Bruttopalkka = Double.Parse(Console.ReadLine());
 
+                    double Bruttopalkka;
+                    string input;
+                    do
+                    {
+                        Console.WriteLine("Bruttopalkka:");
+                        input = Console.ReadLine();
+                        if (!Double.TryParse(input, out Bruttopalkka))
+                        {
+                            Console.WriteLine("Virheellinen syöte. Kokeile uudelleen.");
+                        }
+                    }
+                    while (Bruttopalkka <= 0);
+                      
                     string rivi = Sukunimi + ";" + Etunimi + ";" + Henkilotunnus + ";" + Bruttopalkka;
 
                     // Tie tiedostoon henkilokunta.csv, joka sijaitsee projektin kansiossa
                     string tie = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, Path.GetFileName("henkilokunta.csv"));
 
-                    // Varmistaa halutaanko tiedot lisätä tiedostoon
-                    Console.WriteLine("Haluatko lisätä seuraavat tiedot tiedostoon:" + rivi);
-                    Console.WriteLine("[K] Kyllä");
-                    Console.WriteLine("[E] Ei");
-                    var teksti = Console.ReadLine();
+                    var teksti = "123";
 
-                    if (teksti == "K")
+                    // Varmistaa halutaanko tiedot lisätä tiedostoon
+                    
+                    while (teksti != "K" || teksti != "E") 
                     {
-                        // Lisää rivin tiedostoon
-                        using (StreamWriter sw = File.AppendText(tie))
+                        Console.WriteLine("Haluatko lisätä seuraavat tiedot tiedostoon:" + rivi);
+                        Console.WriteLine("[K] Kyllä");
+                        Console.WriteLine("[E] Ei");
+                        teksti = Console.ReadLine().ToUpper();
+
+                        if (teksti == "K")
                         {
-                            sw.WriteLine(rivi);
+                            // Lisää rivin tiedostoon
+                            using (StreamWriter sw = File.AppendText(tie))
+                            {
+                                sw.WriteLine(rivi);
+                            }
+                            Console.WriteLine("Tiedot lisätty tiedostoon: " + tie);
+                            Console.WriteLine("");
+                            break;
                         }
-                        Console.WriteLine("Tiedot lisätty tiedostoon: " + tie);
-                        Console.WriteLine("");
+                        else if (teksti == "E")
+                        {
+                            Console.WriteLine("Tietoja ei tallennettu.");
+                            Console.WriteLine("");
+                            break;
+                        }
                     }
-                    else if (teksti == "E")
-                    {
-                        Console.WriteLine("Tietoja ei tallennettu.");
-                        Console.WriteLine("");
-                    }
+                          
                 }
-                if (syote == 2)
+                if (syote == "2")
                 {
 
                     Console.WriteLine("Anna poistettavan henkilön henkilötunnus:");
@@ -101,12 +124,16 @@ namespace Palkanlaskenta_projekti
                         Console.WriteLine("");
                     }
                 }
-                if (syote == 3)
+                if (syote == "3")
                 {
                     string tie = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, Path.GetFileName("henkilokunta.csv"));
                     string[] Lines = File.ReadAllLines(tie);
                     Console.WriteLine(String.Join("\n", Lines));
                     Console.WriteLine("");
+                }
+                else
+                {
+                    syote = "99";
                 }
 
             }
